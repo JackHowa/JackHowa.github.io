c85e023a9ba17e1c2493713671de192f87fbec95
Jack Howard <jackhowa@fastmail.com>
2021-12-28 18:21:18 +0000

    perf: Add minification styles performance

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,21 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('manifest.json')
 
   eleventyConfig.addFilter("cssmin", function (code) {
-    return new CleanCSS({}).minify(code).styles;
+    const output = new CleanCSS({
+      level: {
+        2: {
+          // https://www.npmjs.com/package/clean-css#level-2-optimizations
+          all: true, // sets all level 1 and 2 optimization values to `true`
+        }
+      }
+    }).minify(code);
+
+    console.log('******css min perf*******')
+
+    console.log('original size', output.stats.originalSize); // original content size after import inlining
+    console.log('minified size', output.stats.minifiedSize); // optimized content size
+    console.log('efficiency', output.stats.efficiency); // `(originalSize - minifiedSize) / originalSize`, e.g. 0.25 if size is reduced from 100 bytes to 75 bytes
+
+    return output.styles;
   });
 };
